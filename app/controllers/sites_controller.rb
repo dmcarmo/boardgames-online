41369335393de55f4cec4f556e11e05141f3c7bd
David Carmo <dmcarmo@gmail.com>
2020-08-03 20:27:31 +0100

    Changes in sites controller and view
    Added views for sites index and show
    Added CRUD and scraper methods in sites controller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,90 @@
 class SitesController < ApplicationController
+
+  before_action :set_site, only: [:show, :edit, :update, :update_games, :destroy]
+
+  def index
+    @sites = Site.all
+  end
+
+  def show
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    @site.save
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def update_games
+    page = get_page(@site.url)
+    results = get_games(page)
+    results.keys.each do |item|
+      # find game in Games
+      # if game doesn't exist, create it in Games
+      if Game.find_by(name: item.to_s).nil?
+        Game.create(name: item.to_s, bgg_id: results[item][:bgg_id], bgg_url: results[item][:bgg_url])
+      end
+      game = Game.find_by(name: item.to_s)
+      # add site_ID to game in Games and push game_ID to site.games_list
+      # game[:sites_list].push(@site.id)
+      @site[:games_list].push(game[:id])
+    end
+  end
+
+  def destroy
+    @site = @site.destroy
+  end
+
+  def get_games(page)
+    results = {}
+    case @site.name
+    when "Boiteajeux"
+      boiteajeux(page, results)
+    else
+      puts "Implement parsing logic for #{@site.name}"
+    end
+  end
+
+  def boiteajeux(page, results)
+    page.search('.jeuxRegles').each do |element|
+      name = element.xpath('text()').text.strip.capitalize # replace all spaces with underscores
+      bgg_search = element.search('@href').select{|link| link.text.include?("geek")}
+      if bgg_search.empty?
+        bgg_url = ""
+        bgg_id = ""
+      else
+        bgg_url = element.search('@href').select{|link| link.text.include?("geek")}.last.text
+        bgg_id = bgg_url.split("/")[-2]
+      end
+      results[name.to_sym] = { bgg_url: bgg_url, bgg_id: bgg_id }
+    end
+    results
+  end
+
+  private
+
+  def set_site
+    @site = Site.find(params[:id])
+  end
+
+  def get_page(url)
+    page = ""
+    begin
+      page = Nokogiri::HTML(open(url).read)
+    rescue Timeout::Error
+      puts "Timeout connecting to #{url}"
+    rescue SocketError
+      puts "Unknown server - #{url}"
+    end
+    page
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'games#index'
+  get '/sites', to: 'sites#index'
+  get '/sites/:id', to: 'sites#show', as: 'site'
+  get '/sites/:id/updategames', to: 'sites#update_games', as: 'update_games'
 end
