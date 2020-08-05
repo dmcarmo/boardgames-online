6a6e11f34a276b5f512f64ffe6b69034b9a4dbf2
David Carmo <dmcarmo@gmail.com>
2020-08-05 02:04:27 +0100

    Changed several files to make the search feature working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,15 @@ class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @games = Game.all
+    if params["query"].present?
+      @games = Game.all.search_by_name(params["query"])
+    else
+      @games = Game.all
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
