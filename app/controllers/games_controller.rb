303f6cb01285888276e064a71a3b3d2bda5073a5
David Carmo <dmcarmo@gmail.com>
2021-07-05 20:09:00 +0100

    Adds pagination with the pagy gem

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,20 +3,21 @@ class GamesController < ApplicationController
 
   def index
     @sites = Site.includes(:games)
-    @games = Game.includes(:sites)
+    @pagy, @games = pagy(Game.includes(:sites))
     if params["filter"].present?
       if params["filter"]["sites"].present?
         site_ids = params["filter"]["sites"].map {|s| s.to_i}
         game_ids = Site.where(id: site_ids).map{ |site| site.games.pluck(:id) }.flatten.uniq
-        @games = @games.where(id: game_ids).includes(:sites)
+        @pagy, @games = pagy(@games.where(id: game_ids).includes(:sites))
       end
       if params["filter"]["search"].present?
         query = params["filter"]["search"]
-        @games = @games.search_by_name(query).includes(:sites)
+        @pagy, @games = pagy(@games.search_by_name(query).includes(:sites))
       end
     end
     puts @games
     puts @sites
+    puts @pagy
     respond_to do |format|
       format.html
       format.js
